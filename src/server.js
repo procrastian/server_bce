cbf3e718256a137074b019bc0aea0c5fa180cadd
Your Name <your_email@address>
2023-08-22 18:30:42 +0100

    express set up, package.json updated

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
