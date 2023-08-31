13450c06a521e81063d0596cb5af3969a230dc1e
Your Name <your_email@address>
2023-08-31 20:20:59 +0100

    seed course images updated

--- prisma/seed.js
+++ prisma/seed.js
@@ -12,7 +12,7 @@ async function seed() {
     "Learn techniques on how to calm the mind, and build inner peace and confidence",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1474418397713-7ede21d49118?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1153&q=80",
     ["Mindfullness", "Meditation", "YogaPractice"]
   );
   await createCourse(
@@ -20,7 +20,7 @@ async function seed() {
     "Learn techniques on how to calm the mind, and build inner peace and confidence",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1527212986666-4d2d47a80d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80",
     ["Mindfullness", "Meditation", "YogaPractice", "UnderFive"]
   );
   await createCourse(
@@ -28,7 +28,7 @@ async function seed() {
     "Learn techniques on how to calm the mind, and build inner peace and confidence",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1505455184862-554165e5f6ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
     ["Mindfullness", "Meditation", "YogaPractice", "PrimaryAge"]
   );
   await createCourse(
@@ -36,7 +36,7 @@ async function seed() {
     "Learn techniques on how to calm the mind, and build inner peace and confidence",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1601925260368-ae2f83cf8b7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1160&q=80",
     ["Mindfullness", "Meditation", "YogaPractice", "SecondaryAge"]
   );
   await createCourse(
@@ -44,7 +44,7 @@ async function seed() {
     "Immerse you and your baby in nature. Enjoy freshly made herbal tea, parent support and a nature based baby sensory opportunity for your little one. (For pre walkers)",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1471342051519-9621d25323fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
     ["UnderFive"]
   );
   await createCourse(
@@ -52,7 +52,7 @@ async function seed() {
     "Sign up for a consultation to understand your child's needs and then receive a bespoke tutoring plan. Available for KS1 to KS5 students, students with EHCPs, home educated students and students out of school receiving funding for tutoring.Available online and in person at Base Camp Education, and also in schools/setting of your choice.",
     4,
     "01/09/2023",
-    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1630983358494-96012d838b84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
     ["PrimaryAge", "SecondaryAge"]
   );
 
